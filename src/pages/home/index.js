f718513e46c6319d8882ef2875836da03cae5241
LucasPereira9 <lucas970997@gmail.com>
2024-05-28 15:11:08 -0300

    feat: input made

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,15 +3,30 @@ import { connect } from 'react-redux';
 import { toggleModalState, toggleTheme } from '../../actions';
 import styles from './home.module.css'
 import Header from '../../components/header'
+import Input from '../../components/input'
 
 const App = (props) => {
+
+  const [searchItem, setSearchItem] = React.useState("");
+  
+  const handleChange = (event) => {
+    setSearchItem(event.target.value);
+};
+
+
   return (
-    <body className={`${styles.light_container} ${props.theme === 'dark' ? styles.dark_container : ''}`}>
+    <div className={`${styles.light_container} ${props.theme === 'dark' ? styles.dark_container : ''}`}>
       <Header />
       <div className={styles.top_container}>
-        <h1>GitExplorer</h1>
+        <h1 className={`${styles.title} ${props.theme === 'dark' ? styles.title_dark : ''}`}>GitExplorer</h1>
+        <Input 
+          value={searchItem}
+          placeholder="Procurar Repositório..."
+          onChange={handleChange}
+          submit={() => console.log('deuuu', searchItem)}
+        />
       </div>
-    </body>
+    </div>
   );
 };
 
